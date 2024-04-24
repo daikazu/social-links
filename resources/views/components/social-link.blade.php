2c169d561c521a6e30793c15ec21f72003bd7f78
Mike Wall <daikazu@gmail.com>
2024-04-24 15:08:08 -0400

    🩹 Fix social link URL in blade component
    The previous version of the social-link.blade.php was appending the name to the URL, which was causing incorrect links to be generated.

--- resources/views/components/social-link.blade.php
+++ resources/views/components/social-link.blade.php
@@ -6,7 +6,7 @@
 @endphp
 
 @if($url)
-    <a href="{{ $url }}{{ $name }}" title="Visit us on {{ ucfirst($platform) }}" {{ $attributes }}>
+    <a href="{{ $url }}" title="Visit us on {{ ucfirst($platform) }}" {{ $attributes }}>
         @include($iconView, ['classes' => $iconStyles])  <!-- Include the icon view with optional classes -->
         <span class="sr-only">{{ ucfirst($platform) }}</span>
     </a>
